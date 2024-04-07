b76843e2074b5d507a843e71dbfbc40646059f58
Tom Plant <tom@tplant.com.au>
2024-04-07 09:35:05 +0000

    feat: add Namespace CLI via Github Releases
    Signed-off-by: Tom Plant <tom@tplant.com.au>

---
+++ test/namespace-cli/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -i
+
+set -e
+
+source dev-container-features-test-lib
+
+check "nsc version" nsc version
+check "docker-credential-nsc --help" docker-credential-nsc --help
+
+reportResults
